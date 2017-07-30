aec4f2abe959c1d39c8a8776bb11cbb5ba7f5cb7
Ronen Malka <ronen1malka@gmail.com>
2017-07-30 22:44:56 +0300

    entities

---
+++ src/entities/Book.ts
@@ -0,0 +1,11 @@
+﻿export class Book {
+    dankode: string = "";
+    class: string = "";
+    name: string = "";
+    type: string = "";
+    author: string = "";
+    publisher: string = "";
+    price: number = 0.0;
+    comment: string ="";
+    timeStamp: Date = new Date();
+}
